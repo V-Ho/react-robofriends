d17e2a540876f032fd4146db6f2fad5133d5f4ef
VHo <vanessa.lg.ho@gmail.com>
2019-08-15 15:44:56 +0200

    map over robot array to display profiles

--- src/CardList.js
+++ src/CardList.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Card from './Card'
 
 const CardList = ({ robots }) => {
+
+    const cardComponent = robots.map((currVal, i) => {
+        return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+    })
     return (
         <div>
-            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-            <Card id={robots[1].id} name={robots[1].name} email={robots[0].email} />
-            <Card id={robots[2].id} name={robots[2].name} email={robots[0].email} />
+            {cardComponent}
         </div>
     )
 }
