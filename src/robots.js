11a744f96e665902da43f2ee328b4229b47649d4
VHo <vanessa.lg.ho@gmail.com>
2019-08-15 14:12:29 +0200

    dynamically render cards with different profiles

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Vanessa Ho',
+        username: 'vho',
+        email: 'vho@mail.com'
+    },
+    {
+        id: 2,
+        name: 'Hazel Ho',
+        username: 'haze',
+        email: 'haze@mail.com'
+    },{
+        id: 3,
+        name: 'Victoria Ho',
+        username: 'vick',
+        email: 'vick@mail.com'
+    },{
+        id: 4,
+        name: 'Ben Ho',
+        username: 'ben',
+        email: 'ben@mail.com'
+    },{
+        id: 5,
+        name: 'Eliza Ho',
+        username: 'esho',
+        email: 'esho@mail.com'
+    },{
+        id: 6,
+        name: 'Bertha',
+        username: 'bigb',
+        email: 'bigb@mail.com'
+    },{
+        id: 7,
+        name: 'Smokey Ho',
+        username: 'smokey',
+        email: 'smokey@mail.com'
+    },{
+        id: 8,
+        name: 'Meow Ho',
+        username: 'meow',
+        email: 'meow@mail.com'
+    },
+]
