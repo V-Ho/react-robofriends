2964bfb6d02885a4747fe98a7be75685b8165caa
VHo <vanessa.lg.ho@gmail.com>
2019-08-15 14:01:36 +0200

    first styled cards component

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ import 'tachyons'
 
 // ReactDOM.render(<Hello greeting={'Meow' + 'React Ninja'} />, document.getElementById('root'));
 
-ReactDOM.render(<Card/>, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <Card/>
+        <Card/>
+        <Card/>
+    </div>,
+    document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
