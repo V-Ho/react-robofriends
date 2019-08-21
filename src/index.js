30292f1a2cae1b44db29039c17da9c493b3fb28c
VHo <vanessa.lg.ho@gmail.com>
2019-08-21 12:21:32 +0200

    pass store as prop to Provider component in app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons'
 import App from './containers/App'
+import { searchRobots } from './reducers';
 
+const store = createStore(searchRobots)
+
+// Pass in store as a prop to Provider component to get state
 ReactDOM.render(
-      <App/>,
+      <Provider store={store}>
+        <App />
+      </Provider>,
     document.getElementById('root'))
 
 serviceWorker.unregister();
